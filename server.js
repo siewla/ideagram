466f132e9b4a12ed97a4294901a6de5e733c9720
siewla <siewlapang@gmail.com>
2020-06-10 19:18:54 +0800

    initial set up

---
+++ server.js
@@ -0,0 +1,30 @@
+/*------------DEPENDENCIES------------*/ 
+const express           = require('express');
+const methodOverride    = require('method-override');
+const app               = express ();
+const port              = process.env.PORT || 3000;
+// const db                = require('./database');
+
+/*------------MIDDLEWARE------------*/ 
+app.use(methodOverride('_method'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.json()); //for parsing application/json 
+app.use(express.urlencoded({ extended: false }));
+
+/*------------Route------------*/
+app.get('/', async (req, res)=>{
+    res.send('Hello World');
+});
+
+
+// require('./routes')(app);
+
+/*------------LISTENER------------*/ 
+// db.connect().then(()=>{
+//     app.emit('ready');
+// });
+
+app.listen(port, () =>{
+    console.log(`IDEAGRAM is listening on port : ${port}`);
+});
