2a7427b1d72c99c26b95cb57428a1aa0db63769c
siewla <siewlapang@gmail.com>
2020-06-11 21:29:06 +0800

    explore cookie

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /*------------DEPENDENCIES------------*/ 
 const express           = require('express');
 const methodOverride    = require('method-override');
+const cookieParser      = require('cookie-parser');
 const app               = express ();
 const port              = process.env.PORT || 3000;
 // const db                = require('./database');
@@ -11,6 +12,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.json()); //for parsing application/json 
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 /*------------Route------------*/
 require('./routes')(app);
