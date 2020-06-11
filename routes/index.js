a52222d83a9077f2dad0183090374008ecb76778
siewla <siewlapang@gmail.com>
2020-06-11 20:22:23 +0800

    drafted css for registration form

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,8 @@ module.exports = (app) => {
     app.get('/', async (req, res)=>{
         res.render('ideagram/mainpage.ejs');
     });
+
+    app.get('/register', async (req, res)=>{
+        res.render('ideagram/registration.ejs');
+    });
 };
